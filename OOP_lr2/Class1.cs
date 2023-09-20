95565fafeab586469a593a4559eaac1d6e89b820
Oleg Stepanenko <s.gelo2004@gmail.com>
2023-09-20 16:55:52 +0300

    first commit

---
+++ OOP_lr2/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_lr2 {
+    internal class Class1 {
+        public void Func() {
+            Console.WriteLine("Goodbye, World!");
+        }
+    }
+}
---
+++ OOP_lr2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_lr2 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello, world!");
+            int a = 5, b = 6;
+            int sum = a + b;
+            Console.WriteLine(sum);
+
+            var class1 = new Class1();
+            class1.Func();
+        }
+    }
+}
